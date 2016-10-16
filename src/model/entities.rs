784a4e508c7b0d21c78764e6b8a9512b04a79fed
chridou <chridou@users.noreply.github.com>
2016-10-16 22:10:28 +0200

    added customer

--- src/model/entities.rs
+++ src/model/entities.rs
@@ -3,5 +3,18 @@ use uuid::Uuid;
 #[derive(Debug, Serialize, Deserialize, Queryable)]
 pub struct User {
     id: Uuid,
+    username: String,
+    surname: String,
+    last_name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+pub struct Customer {
+    id: Uuid,
     name: String,
+    address_line_1: String,
+    address_line_2: Option<String>,
+    address_zip: Option<String>,
+    address_city: Option<String>,
+    address_country: Option<String>,
 }
